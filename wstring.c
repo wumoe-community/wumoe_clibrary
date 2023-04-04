a0279c9d9dffe316edfb0d425fe0261a7bbb23cc
RMOlive <rmolives@163.com>
2023-04-04 16:50:23 +0800

    wstring

--- wstring.c
+++ wstring.c
@@ -9,7 +9,7 @@ char *__concat__(size_t argc, char** argv) {
     size_t total_len = 1, len[argc];
     for (size_t i = 0; i < argc; ++i)
         total_len += (len[i] = strlen(argv[i]));
-    char* dest = malloc(total_len * sizeof(char));
+    char *dest = malloc(total_len * sizeof(char));
     for (size_t i = 0, offset = 0; i < argc; offset += len[i], ++i)
         strcpy(dest + offset, argv[i]);
     return dest;
